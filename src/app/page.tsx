2b6c2d33e93c274f26a897f9f9baf2096aea872b
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-27 15:57:56 +0600

    next app tanvir shop first contribute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="bg-black text-white text-3xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+      distinctio facere quaerat vitae eius reprehenderit sunt eveniet numquam
+      nihil nesciunt!
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
