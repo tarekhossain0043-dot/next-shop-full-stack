27ecf9c846fa92ac0321a5c3b2194c69c27d61fe
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-29 14:54:01 +0600

    feature/top product added on there

--- src/assets/all_data.js
+++ src/assets/all_data.js
@@ -1,6 +1,9 @@
 // slider data
 
+import { Eye, Heart, ShoppingCart, Star } from "lucide-react";
 import { BsBoxSeam } from "react-icons/bs";
+import {nanoid} from 'nanoid'
+
 
 // import sliderImg from '/slider/slider-1.png'
 export const hero_slider = [
@@ -29,6 +32,7 @@ export const hero_slider = [
     number: "$299",
   },
 ];
+
 
 export const top_search_Pro = [
   {
@@ -67,3 +71,182 @@ export const features = [
     subTitle: "Delivery in 24/H",
   },
 ];
+
+const stars = Array(5).fill(null).map((_,i) => (
+  <Star key={i} />
+))
+console.log(stars);
+const id = nanoid();
+
+export const topProducts = [
+  {
+    id : id,
+    offer : '32% OFF',
+    type : 'HOT',
+    category : 'electronics',
+    img : '/products/1gr.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Xbox Series S - 512GB SSD Console with Wireless Controller - EU Versio...',
+    price : '$442.12',
+    delPrice : '$865.99',
+    desc : 'Games built using the Xbox Series X|S development kit showcase unparalleled load times, visuals.',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    inStork : 'SOLD OUT',
+    type : 'HOT',
+    category : 'electronics',
+    img : '/products/1g.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Bose Sport Earbuds - Wireless Earphones - Bluetooth In Ear...',
+    price : '$442.12',
+    delPrice : '$865.99',
+    desc : 'Games built using the Xbox Series X|S development kit showcase unparalleled load times, visuals.',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    offer : '19% OFF',
+    type : 'HOT',
+    category : 'electronics',
+    img : '/products/1g.png',
+    star : stars,
+    star_num : '52,677',
+    title : '4K UHD LED Smart TV with Chromecast Built-in',
+    price : '$1,50',
+    delPrice : '$865',
+    desc : 'Games built using the Xbox Series X|S development kit showcase unparalleled load times, visuals.',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    type : 'HOT',
+    category : 'electronics',
+    img : '/products/1g.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Simple Mobile 4G LTE Prepaid Smartphone',
+    price : '$2,20',
+    desc : 'Games built using the Xbox Series X|S development kit showcase unparalleled load times, visuals.',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    category : 'electronics',
+    img : '/products/1p.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Bose Sport Earbuds - Wireless Earphones - Bluetooth In Ear...',
+    price : '$442.12',
+    delPrice : '$1200',
+    desc : 'Sony DSCHX8 High Zoom Point & Shoot Camera',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    category : 'electronics',
+    img : '/products/1p.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Dell Optiplex 7000x7480 All-in-One Computer Monitor',
+    price : '$2,99',
+    delPrice : '$700',
+    desc : 'Sony DSCHX8 High Zoom Point & Shoot Camera',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    category : 'electronics',
+    img : '/products/1p.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'Portable Wshing Machine, 11lbs capacity Model 18NMFIAM',
+    price : '$570',
+    delPrice : '$870.99',
+    desc : 'Sony DSCHX8 High Zoom Point & Shoot Camera',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    type : 'HOT',
+    category : 'electronics',
+    img : '/products/1p.png',
+    star : stars,
+    star_num : '52,677',
+    title : '2-Barrel Carburetor Carb 2100 Engine Increase Horsepower',
+    price : '$160',
+    delPrice : '$370.99',
+    desc : 'Sony DSCHX8 High Zoom Point & Shoot Camera',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+  {
+    id : id,
+    offer : '32% OFF',
+    category : 'electronics',
+    img : '/products/1p.png',
+    star : stars,
+    star_num : '52,677',
+    title : 'JBL FLIP 4 - Waterproof Portable Bluetooth Speaker - Black',
+    price : '$250',
+    delPrice : '$360.99',
+    desc : 'Sony DSCHX8 High Zoom Point & Shoot Camera',
+    icons : [
+      {
+        Heart,
+        Eye,
+        ShoppingCart
+      }
+    ]
+  },
+]
